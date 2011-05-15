3e8e0b961fbe13d8277a59a59e00308a0b786701
Andrey Koppel <akoppela@gmail.com>
2011-05-15 13:23:08 +0800

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamepon::Application.config.secret_token = 'ed2439d5ee224c7180851731510f08def0bf477ed75e567084e00ea0968f05e8ae0028bc02591ad50f8cf6442ae041f8145e42ac51758887d507a9046ff7d8b4'
