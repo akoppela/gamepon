ccc867c641b8a282cd4f2a816932500d0c68adde
Andrey Koppel <akoppela@gmail.com>
2011-05-15 13:40:03 +0800

    + start app

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Gamepon::Application.initialize!
+
+Sass::Plugin.options[:template_location] = 'app/stylesheets'
